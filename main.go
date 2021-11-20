fa48ba0d6ee48ec3d025b2930954d9b8f4699546
Mattio <matiasb.murga@gmail.com>
2021-11-20 20:15:24 -0300

    Queues package added

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/elMattio/golang-data-structures.git/hashtables"
+	"github.com/elMattio/golang-data-structures.git/queues"
 )
 
 func main() {
 	hashtables.InitHashTableExample()
+	queues.InitQueueExample()
 }
---
+++ queues/queue.go
@@ -0,0 +1,7 @@
+package queues
+
+import "fmt"
+
+func InitQueueExample() {
+	fmt.Println("Iniciando ejemplo de una queue")
+}
